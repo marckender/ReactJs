cff5d325cec0048709dad480577dba2157e0585a
makender103 <makender103@gmail.com>
2020-09-17 19:03:15 -0300

    final version 1

--- coctail_project/src/components/SearchForm.js
+++ coctail_project/src/components/SearchForm.js
@@ -1,5 +1,35 @@
 import React from "react";
 
-export default function SearchForm() {
-  return <h1>searchform component</h1>;
+export default function SearchForm({setSearchTerm}) {
+
+  const searchValue = React.useRef("");
+
+  React.useEffect(()=> {
+    searchValue.current.focus()
+  }, []);
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+  }
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value)
+  }
+  return (
+    <section className="section">
+      <h2 className="section-title">search coctails</h2>
+
+      <form action="" className="form search-form" onSubmit= {handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="namesearch your favorites coctail"></label>
+          <input 
+            type="text"
+            name="name"
+            id="name"
+            onChange={searchCocktail} 
+            ref= {searchValue}
+          />
+        </div>
+      </form>
+    </section>
+  )
 }
