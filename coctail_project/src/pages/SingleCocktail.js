1aac08db2efb6e01bd46ceeee7f50614a101ee99
makender103 <makender103@gmail.com>
2020-09-17 18:46:55 -0300

    the Single Cocktail page, context api

--- coctail_project/src/pages/SingleCocktail.js
+++ coctail_project/src/pages/SingleCocktail.js
@@ -1,9 +1,98 @@
 import React from "react";
-import {useParams} from 'react-router-dom'
+import {useParams, Link} from 'react-router-dom'
 
 export default function SingleCocktail() {
   const {id} = useParams();
 
+  const [loading, setLoading]= React.useState(false);
 
-return <h1>single Coctail page: {id}</h1>;
+  const [cocktail, setCocktail]=  React.useState(null);
+
+  React.useEffect(()=> {
+    setLoading(true);
+    async function getCocktail() {
+      try{
+        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+
+        const data = await response.json();
+      if (data.drinks) {
+        const { strDrink: name,
+                strDrinkThumb:image,
+                strAlcoholic: info,
+                strCategory:category,
+                strGlass: glass,
+                strInstructions: instructions,
+                strIngredient1, strIngredient2,
+                strIngredient3, strIngredient4,
+                strIngredient5
+              } = data.drinks[0];
+
+        const ingredients = [strIngredient1, strIngredient2,
+                            strIngredient3, strIngredient4,
+                            strIngredient5];
+        
+        const newCocktail = {
+          name,
+          image,
+          info,
+          category,
+          glass,
+          instructions,
+          ingredients
+        };
+        setCocktail(newCocktail);  
+      } else {
+        setCocktail(null);
+      }
+
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+    getCocktail()
+  },[id]);
+
+  if (loading) {
+    return <h2 className="section-title"> Loading ...</h2>;
+  }
+
+  if (!cocktail) {
+    return <h2 className="section-title">no Coctail to display</h2>;
+  } else {
+      const { name,
+          image,
+          info,
+          category,
+          glass,
+          instructions,
+          ingredients
+      }= cocktail;
+      return (
+        <section className="section cocktail-section">
+          <Link to="/" className="btn btn-primary"> Back Home</Link>
+          <h2 className="section-title">{name}</h2>
+          <div className="drink">
+            <img src={image} alt={name}/>
+            <div className="drink-info">
+              <p> name: {name}</p>
+              <p> image: {image} </p>
+              <p> info: {info} </p>
+              <p> category: {category}</p>
+              <p> glass: {glass}</p>
+              <p> instructions:{instructions} </p>
+              <p> 
+                ingredients: {
+                  ingredients.map((item, index)=> {
+                  return item ? <span key={index}> {item}</span> 
+                    : null;
+                  })
+                }
+              </p>
+            </div>
+          </div>
+
+      </section>
+    );
+} 
 }
